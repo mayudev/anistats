572a683de2935790fd6e6ed23b6395e92a078089
pizza61 <34103636+pizza61@users.noreply.github.com>
2021-07-27 20:30:18 +0200

    eu

--- src/components/Special/About.vue
+++ src/components/Special/About.vue
@@ -3,8 +3,6 @@
         <h2>About</h2>
         <p>anistats is an extension for AniList that lets you check how much time you spend/waste on watching anime and reading manga based on your activities.</p>
         <p>Source code of this site is available <a href="https://github.com/pizza61/anistats2">here</a>.</p>
-        <h2>Privacy</h2>
-        <p>This site does not collect any information about you. There is no Google Analytics in here or whatever, we only use Google Fonts but AniList does as well. Also, it's hosted on GitHub Pages so you may check their privacy policy or something. The only information stored locally on your computer is whether you enabled dark mode or not. If you're a few bytes short of space then feel free to delete it. Anyways, please don't sue me to the EU.</p>
     </div>
 </template>
 
