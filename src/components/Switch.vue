a870b57fac7101be52f1b036825931c29374af03
pizza61 <34103636+pizza61@users.noreply.github.com>
2021-09-09 23:20:16 +0200

    Changed switch color

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -65,7 +65,7 @@ export default class Switch extends Vue {
 }
 
 .switch-input:checked + .switch-label:after {
-    background-color: var(--color-text-blue);
+    background-color: var(--color-selected-text);
     transform: translate(80%, -50%);
 }
 </style>
