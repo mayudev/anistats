0aa7d9d9499caa09d21ce8a33545eec631a7800a
mayudev <99561108+mayudev@users.noreply.github.com>
2022-06-07 22:45:48 +0200

    feat(home): add header icon

---
+++ src/components/Header/HeaderBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import SmallBadge from '../layout/SmallBadge.vue'
+import HeaderIcon from './HeaderIcon.vue'
+</script>
+
+<template>
+  <nav>
+    <SmallBadge error>beta</SmallBadge>
+    <HeaderIcon icon="circle-half-stroke" alt="Toggle theme" />
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+}
+</style>
---
+++ src/components/Header/HeaderIcon.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import FontAwesomeIcon from '../FontAwesomeIcon.vue'
+
+const props = defineProps<{
+  icon: string
+  alt: string
+}>()
+</script>
+
+<template>
+  <button :title="alt" :aria-label="alt" class="icon-button">
+    <FontAwesomeIcon :width="16" :icon="props.icon"></FontAwesomeIcon>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.icon-button {
+  all: unset;
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0 0.5rem;
+  padding: 0.5rem;
+  border-radius: 50%;
+}
+</style>
