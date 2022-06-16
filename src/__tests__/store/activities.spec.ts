f6578b5d01820df8d6a2e421bcecf57d156a5fe8
mayudev <99561108+mayudev@users.noreply.github.com>
2022-06-16 19:40:59 +0200

    test: test parseActivities for multiple days

--- src/__tests__/store/activities.spec.ts
+++ src/__tests__/store/activities.spec.ts
@@ -60,6 +60,44 @@ const dataset: UserActivity[] = [
     status: 'watched episode',
     progress: '5 - 12',
   },
+  {
+    __typename: 'ListActivity',
+    id: 212432111,
+    media: {
+      id: 4224,
+      type: 'ANIME',
+      title: {
+        romaji: 'Toradora!',
+      },
+      coverImage: {
+        medium:
+          'https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/4224.jpg',
+      },
+    },
+    type: 'ANIME_LIST',
+    createdAt: 1654312162,
+    status: 'watched episode',
+    progress: '1 - 3',
+  },
+  {
+    __typename: 'ListActivity',
+    id: 23321213,
+    media: {
+      id: 5597,
+      type: 'ANIME',
+      title: {
+        romaji: 'Natsu no Arashi!',
+      },
+      coverImage: {
+        medium:
+          'https://s4.anilist.co/file/anilistcdn/media/anime/cover/small/5597.jpg',
+      },
+    },
+    type: 'ANIME_LIST',
+    createdAt: 1654312162,
+    status: 'watched episode',
+    progress: '1 - 4',
+  },
 ]
 
 describe('parseActivities', () => {
@@ -68,11 +106,18 @@ describe('parseActivities', () => {
 
     const keys = Array.from(parsed.keys())
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const day = parsed.get(keys[0])!
+    const day1 = parsed.get(keys[0])!
 
-    expect(day.totalEpisodes).toBe(10)
-    expect(day.media).toHaveLength(2)
-    expect(day.media[0].progress).toBe(1)
-    expect(day.media[1].progress).toBe(9)
+    expect(day1.totalEpisodes).toBe(10)
+    expect(day1.media).toHaveLength(2)
+    expect(day1.media[0].progress).toBe(1)
+    expect(day1.media[1].progress).toBe(9)
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const day2 = parsed.get(keys[1])!
+    expect(day2.totalEpisodes).toBe(7)
+    expect(day2.media[0].progress).toBe(3)
+    expect(day2.media[0].title.romaji).toBe('Toradora!')
+    expect(day2.media[1].progress).toBe(4)
   })
 })
