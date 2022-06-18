cfa7bcc6b27df945b4d6bd80d6b45d57a3973984
mayudev <99561108+mayudev@users.noreply.github.com>
2022-06-18 10:02:59 +0200

    fix: incorrect month display

--- src/lib/days.ts
+++ src/lib/days.ts
@@ -10,11 +10,11 @@ export const weekdays = [
 
 export function displayDate(date: Date): string {
   const d = date.getDate()
-  const m = date.getMonth()
+  const m = date.getMonth() + 1
   const y = date.getFullYear()
 
   const dom = d >= 10 ? String(d) : '0' + String(d)
   const month = m >= 10 ? String(d) : '0' + String(m)
 
-  return dom + '/' + month + '/' + y
+  return month + '/' + dom + '/' + y
 }
