d0a25d3a5acbd563cebfe9ec5b773475e9b0c708
mayudev <99561108+mayudev@users.noreply.github.com>
2022-06-18 22:42:29 +0200

    feat: styling for charts

--- src/views/User/OverviewTab.vue
+++ src/views/User/OverviewTab.vue
@@ -12,18 +12,24 @@ const user = useUserStore()
   <main>
     <div>
       <DataRange :timestamp="user.lastDayTimestamp" />
-      <ActivityChart
-        v-if="user.dataset !== 'manga'"
-        name="anime"
-        :days="user.days"
-        property="totalEpisodes"
-      />
-      <ActivityChart
-        v-if="user.dataset !== 'anime'"
-        name="manga"
-        :days="user.days"
-        property="totalChapters"
-      />
+      <div class="chart-container">
+        <div class="chart-header">Episodes watched</div>
+        <ActivityChart
+          v-if="user.dataset !== 'manga'"
+          name="anime"
+          :days="user.days"
+          property="totalEpisodes"
+        />
+      </div>
+      <div class="chart-container">
+        <div class="chart-header">Chapters read</div>
+        <ActivityChart
+          v-if="user.dataset !== 'anime'"
+          name="manga"
+          :days="user.days"
+          property="totalChapters"
+        />
+      </div>
     </div>
     <div>
       <ActivityTimeline :days="user.days" />
@@ -35,5 +41,20 @@ const user = useUserStore()
 main {
   display: grid;
   grid-template-columns: 50% 50%;
+}
+
+.chart-container {
+  box-shadow: 0 5 3px rgba(0, 0, 0, 0.25);
+  margin-bottom: 12px;
+  margin-right: 0.5rem;
+}
+
+.chart-header {
+  background: var(--color-background-secondary);
+  border-radius: 6px;
+  padding: 0.5rem 1rem;
+
+  font-weight: 500;
+  font-size: 1.2rem;
 }
 </style>
