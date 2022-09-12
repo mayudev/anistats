b24990cc2b4b6ebf642b831d48cf70d428312705
mayudev <99561108+mayudev@users.noreply.github.com>
2022-09-12 22:42:16 +0200

    fix: remove the ugly landing page

--- src/components/Home/HomeLanding.vue
+++ src/components/Home/HomeLanding.vue
@@ -1,25 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <article class="landing">
-    <p class="heading">Extended statistics for your AniList profile</p>
-    <p>
-      <strong>anistats</strong> shows you various stats about your anime and
-      manga.
-    </p>
-    <p>
-      Thanks to the <strong>Activity</strong> feature on AniList, you can trace
-      your anime watching history even a few years back! - as long as you didn't
-      have Activity Merge Time set to Always - more info in
-      <RouterLink to="help">help</RouterLink>
-    </p>
-    <p>
-      Statistics are based on <strong>public</strong> data available on your
-      profile. This means your profile <strong>has to be public</strong> for it
-      to work.
-    </p>
-    <p>anistats is not affiliated with AniList.</p>
-  </article>
+  <div></div>
+  <!-- TODO make a landing page that's not ugly -->
 </template>
 
 <style lang="scss" scoped>
