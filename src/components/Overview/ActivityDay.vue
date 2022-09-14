65eec89d744c7d76a57a775d7d859da9049882a0
mayudev <99561108+mayudev@users.noreply.github.com>
2022-09-14 18:20:32 +0200

    style: don't display the additional margin in popups

--- src/components/Overview/ActivityDay.vue
+++ src/components/Overview/ActivityDay.vue
@@ -35,7 +35,7 @@ const close = () => {
 
 <template>
   <div class="wrapper" :class="{ 'wrapper-animated': !props.closeable }">
-    <div class="header">
+    <div class="header" :class="{ 'header-margin': !props.closeable }">
       <div class="header-date">
         <div class="weekday">{{ weekdays[state.date.getDay()] }}</div>
         <div class="date">{{ displayDate(state.date) }}</div>
@@ -94,9 +94,12 @@ const close = () => {
 
   background: var(--color-background-secondary);
   padding: 0.5rem 0.75rem;
-  margin-bottom: 0.5rem;
 
   transition: background var(--theme-transition);
+}
+
+.header-margin {
+  margin-bottom: 0.5rem;
 }
 
 .weekday,
